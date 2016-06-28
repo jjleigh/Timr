71f504f2f46475cc42a5f3c44b59915401e7a1d3
Justine <justinejones91@gmail.com>
2016-06-28 19:55:53 -0400

    tests for state change of minute second and pausing all passing

--- app/components/timr.jsx
+++ app/components/timr.jsx
@@ -9,36 +9,38 @@ class Timr extends React.Component {
     this.state = {
       paused: true,
       minute: this.props.minute,
-      second: this.props.second
+      second: this.props.second,
+      intervalId: 0
     }
-
   }
 
   incrementMinute() {
-    if(this.state.second == 0) {
+    if( this.state.paused == false && this.state.second == 0 ) {
       this.setState({minute: this.state.minute + 1 });
     }   
   }
 
   incrementSecond() {
-    if(this.state.second < 59 ) {
-      this.setState({second: this.state.second + 1 })
-    } else {
-      this.incrementMinute()
-      this.setState({second: 0 });
-    }
-
+    if (this.state.paused == false) {
+      if(this.state.second < 59 ) {
+        this.setState({second: this.state.second + 1 });
+      } else {
+        this.setState({second: 0 });
+        this.incrementMinute()
+      }
+    } 
   }
 
   changePauseState() {
     if(this.state.paused == true) {
-      this.setState({ paused: false })
+      this.setState({ paused: false });
+      let id = setInterval( () => { this.incrementSecond() }, 1000);
+      this.setState({intervalId: id });
     } else { 
-      this.setState({ paused: true })
+      this.setState({ paused: true });
+      clearInterval( this.state.intervalId );
     }
   } 
-
-
 
   AddNewTimr() {
 
@@ -53,14 +55,17 @@ class Timr extends React.Component {
           </span>
           <span className="separator common"> : </span>
           <span className="sec-container common">
-            { this.state.second }
+            { this.state.second.toString().length == 1 ? "0" + this.state.second : this.state.second }
           </span>
         </div>
-        <i  className={ `${ this.state.paused == false ? 'hidden-icon ': '' }fa fa-pause icons` } 
+        <i  className={ `${ this.state.paused == true ? 'hidden-icon ': '' }fa fa-pause icons` } 
             aria-hidden="true" 
             onClick={ () => this.changePauseState() } >
         </i>
-        <i className={ `${ this.state.paused == true ? 'hidden-icon ': '' }fa fa-play icons` } aria-hidden="true" onClick={ () => this.changePauseState() }></i>
+        <i  className={ `${ this.state.paused == false ? 'hidden-icon ': '' }fa fa-play icons` } 
+            aria-hidden="true" 
+            onClick={ () => this.changePauseState() }>
+        </i>
       </div>
     )
   }
--- test/timr.spec.js
+++ test/timr.spec.js
@@ -14,7 +14,7 @@ describe('<Timr />', function () {
   describe('changePauseState', function(){
     describe('when pause is true', function(){
       it('should set the pause state to false', function(){
-        const wrapper = mount(<Timr />);
+        const wrapper = mount(<Timr second={ 2 }/>);
         wrapper.instance().changePauseState();
         expect(wrapper.instance().state.paused).to.eq(false);
       });
@@ -22,50 +22,82 @@ describe('<Timr />', function () {
 
     describe('when pause is false', function(){
       it('should set the pause state to true', function(){
-        const wrapper = mount(<Timr />);
+        const wrapper = mount(<Timr second={ 2 }/>);
         wrapper.instance().changePauseState();
         wrapper.instance().changePauseState();
         expect(wrapper.instance().state.paused).to.eq(true);
+      });
+
+      it('should trigger incrementSecond', function() {
+        const wrapper = mount(<Timr second={ 20 }/>);
+        let clock = sinon.useFakeTimers();
+        wrapper.instance().changePauseState();
+        clock.tick(1000);
+        expect(wrapper.instance().state.second).to.eq(21);
+        clock.restore();
       });
     });
   });
 
   describe('incrementMinute', function(){
-    describe('when second state is less than 59 or less', function(){
-      it('should not change the minute state by one', function () {
-        const wrapper = shallow(<Timr minute={ 1 } second={ 30 } />);
-        console.log(wrapper.instance().state)
+    describe('when paused state is true', function() {
+      it('should not change the minute state', function(){
+        const wrapper = shallow(<Timr minute={ 1 } second={ 0 } />);
         wrapper.instance().incrementMinute()
         expect(wrapper.instance().state.minute).to.eq(1);
       });
     });
 
-    describe('when second state is reset to 0', function(){
-      it('should change the minute state by 1', function() {
-        const wrapper = shallow(<Timr minute={ 1 } second={ 0 }/>);
-        wrapper.instance().incrementMinute()
-        expect(wrapper.instance().state.minute).to.eq(2);
+    describe('when paused state is false', function() {
+      describe('when second state is less than 59 or less', function(){
+        it('should not change the minute state by one', function () {
+          const wrapper = shallow(<Timr minute={ 1 } second={ 30 } />);
+          wrapper.instance().changePauseState();
+          wrapper.instance().incrementMinute()
+          expect(wrapper.instance().state.minute).to.eq(1);
+        });
+      }); 
+
+      describe('when second state is reset to 0', function(){
+        it('should change the minute state by 1', function() {
+          const wrapper = shallow(<Timr minute={ 1 } second={ 0 }/>);
+          wrapper.instance().changePauseState();
+          wrapper.instance().incrementMinute()
+          expect(wrapper.instance().state.minute).to.eq(2);
+        });
       });
-    })
-    
+    });
   });
 
-  describe('IncrementSecond', function(){
-    describe('when the second 58 or less', function() {
-      it('should change the second state by one', function () {
-        const wrapper = shallow(<Timr minute={ 0 } second={ 58 } />);
-        wrapper.instance().incrementSecond();
-        expect(wrapper.instance().state.second).to.eq(59);
+  describe('incrementSecond', function(){
+    describe('when paused state is true', function() {
+      it('should not change the second state', function(){
+        const wrapper = shallow(<Timr minute={ 1 } second={ 30 } />);
+        wrapper.instance().incrementSecond()
+        expect(wrapper.instance().state.second).to.eq(30);
       });
     });
 
-    describe('when the secind is 59', function(){
-      it('should reset to 0', function () {
-        const wrapper = shallow(<Timr minute={ 1 } second={ 59 }/>);
-        wrapper.instance().incrementSecond();
-        expect(wrapper.instance().state.second).to.eq(0);
+    describe('when paused state is false', function() {
+      describe('when the second 58 or less', function() {
+        it('should change the second state by one', function () {
+          const wrapper = shallow(<Timr minute={ 0 } second={ 58 } />);
+          wrapper.instance().changePauseState();
+          wrapper.instance().incrementSecond();
+          expect(wrapper.instance().state.second).to.eq(59);
+        });
+      });
+
+      describe('when the second is 59', function(){
+        it('should reset to 0', function () {
+          const wrapper = shallow(<Timr minute={ 1 } second={ 59 }/>);
+          wrapper.instance().changePauseState();
+          wrapper.instance().incrementSecond();
+          expect(wrapper.instance().state.second).to.eq(0);
+        });
       });
     });
+    
   });
 
 
