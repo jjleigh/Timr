4c9fae06c03731261727d3bffc4c91d8ddeaccb3
Justine <justinejones91@gmail.com>
2016-07-03 09:55:25 -0400

    changed callback name

--- test/clock_input.spec.js
+++ test/clock_input.spec.js
@@ -5,7 +5,7 @@ describe('<ClockInput />', function(){
     describe('onKeyUp', function(){
         it('should trigger its callback function', function(){
             var update_time_callback = sinon.spy(); 
-            const wrapper = mount(<ClockInput callback={ update_time_callback } paused={ true } />);
+            const wrapper = mount(<ClockInput pauseCallback={ update_time_callback } paused={ true } />);
             wrapper.find('input').first().simulate('keyup');
             expect(update_time_callback).calledOnce;
         });
@@ -14,7 +14,7 @@ describe('<ClockInput />', function(){
     describe('handleChange', function(){
         describe('when paused state is true', function(){
             it('it should change the value of the input', function(){
-                const wrapper = mount(<ClockInput callback="bob" paused={ true } />);
+                const wrapper = mount(<ClockInput pauseCallback="bob" paused={ true } />);
                 wrapper.find('input').first().simulate('change', {target: {value: 5}});
                 expect(wrapper.instance().state.minute).to.eq(5);
             });
@@ -23,7 +23,7 @@ describe('<ClockInput />', function(){
 
         describe('when paused state is false', function(){
             it('it should not change the value of the input', function(){
-                const wrapper = mount(<ClockInput callback="bob" paused={ false }/>);
+                const wrapper = mount(<ClockInput pauseCallback="bob" paused={ false }/>);
                 wrapper.instance().setState({second: 1});
                 wrapper.find('input').last().simulate('change', {target: {value: 5}});
                 expect(wrapper.instance().state.second).to.eq(1);
